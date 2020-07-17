fea66b5f71d8343bee1587de8fcf3a5d8c99fe41
Rafael Damasceno <damscn@gmail.com>
2020-07-17 02:01:03 -0300

    Change display order and add option to filter by not completed

--- reminders-menu-bar/Views/ContentView.swift
+++ reminders-menu-bar/Views/ContentView.swift
@@ -4,6 +4,7 @@ import EventKit
 struct ContentView: View {
     @State private var newTask: String = ""
     @State private var remindersStore = RemindersService.instance.getReminders()
+    @State private var isFilterEnabled = true
     
     var body: some View {
         VStack(spacing: 0) {
@@ -20,7 +21,7 @@ struct ContentView: View {
                             .font(.headline)
                             .foregroundColor(Color(reminderList.color))
                             .padding(.top, 5)
-                        ForEach(reminderList.reminders, id: \.calendarItemIdentifier) { reminder in
+                        ForEach(self.filteredReminders(reminderList.reminders), id: \.calendarItemIdentifier) { reminder in
                             ReminderItemView(reminder: reminder)
                         }
                     }
@@ -29,6 +30,26 @@ struct ContentView: View {
             .onAppear {
                 self.remindersStore = RemindersService.instance.getReminders()
             }
+            HStack {
+                Button(action: {
+                    self.isFilterEnabled.toggle()
+                }) {
+                    Image(self.isFilterEnabled ? "circle" : "circle.filled")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(10)
+            .background(Color.darkTheme)
+        }
+    }
+    
+    private func filteredReminders(_ reminders: [EKReminder]) -> [EKReminder] {
+        if isFilterEnabled {
+            return reminders.reversed().filter{ !$0.isCompleted }
+        } else {
+            return reminders.reversed()
         }
     }
 }
