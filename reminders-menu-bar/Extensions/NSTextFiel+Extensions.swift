9bef2d96aa8c9777fc01f14573c74c964f3697c0
Rafael Damasceno <damscn@gmail.com>
2021-10-24 00:47:16 -0300

    Change focusRingType to resolve issue on macOS Monterey - Related to #18

--- reminders-menu-bar/Extensions/NSTextFiel+Extensions.swift
+++ reminders-menu-bar/Extensions/NSTextFiel+Extensions.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
-// Workaround to remove focus ring highlight border from textfield
+// Workaround to remove focus ring highlight border from textfield (on macOS Big Sur)
 // https://stackoverflow.com/questions/59813943/swiftui-remove-focus-ring-highlight-border-from-macos-textfield
 
 extension NSTextField {
     open override var focusRingType: NSFocusRingType {
         get { .none }
-        set { self.focusRingType = newValue }
+        set { } // swiftlint:disable:this unused_setter_value
     }
 }
