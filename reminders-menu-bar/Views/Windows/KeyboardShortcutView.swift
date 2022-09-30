4679c57d257b61bb586b957ed5cf155a327ae592
Daniel <daniel.prolite@gmail.com>
2022-09-29 22:10:30 -0500

    Updating settings width size

--- reminders-menu-bar/Views/Windows/KeyboardShortcutView.swift
+++ reminders-menu-bar/Views/Windows/KeyboardShortcutView.swift
@@ -37,7 +37,7 @@ struct KeyboardShortcutView: View {
         .padding(.top, 16)
         .padding(.bottom, 24)
         .padding(.horizontal, 32)
-        .frame(width: 450, height: 180)
+        .frame(width: 520, height: 180)
         .onAppear(perform: {
             removeFocusFromShortcutRecorder()
         })
