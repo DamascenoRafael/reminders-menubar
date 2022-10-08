d08ea93da31cf64bd31e2dbf611b75e2e3c5fdc9
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-10-08 16:16:36 -0400

    removed my weird changes to selectableview that were already covered

--- reminders-menu-bar/Views/Helpers/SelectableView.swift
+++ reminders-menu-bar/Views/Helpers/SelectableView.swift
@@ -6,23 +6,25 @@ struct SelectableView: View {
     var isSelected: Bool
     var color: Color?
     var withPadding = true
-    var withDot = false
     
     var body: some View {
         if isSelected {
             Image(systemName: "checkmark")
-                .frame(minWidth: 0, minHeight: 0)
         }
         
         let paddingText = !isSelected && withPadding ? "      " : ""
-        Text(paddingText + title)
-            .foregroundColor(color)
+        let coloredDot = color != nil ? Text("●  ").foregroundColor(color) : Text("")
+        
+        Group {
+            Text(paddingText).font(.system(size: 11.9)) +
+            coloredDot +
+            Text(title)
+        }
     }
 }
 
 struct SelectableButton_Previews: PreviewProvider {
     static var previews: some View {
-        HStack{SelectableView(title: "Option", isSelected: true, withDot: true)}
-        HStack{SelectableView(title: "Option", isSelected: false, withDot: true)}
+        SelectableView(title: "Option", isSelected: true)
     }
 }
