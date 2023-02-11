da407a6abf6d2c841065f304e9de5f95595abf9f
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-02-11 21:57:44 +0100

    Ignored Pods files in swiftlint and corrected lint warnings

--- reminders-menu-bar/Models/NLPDateParser.swift
+++ reminders-menu-bar/Models/NLPDateParser.swift
@@ -1,48 +1,46 @@
-//
-//  NLPDateParser.swift
-//  Reminders Menu Bar
-//
-//  Created by Domiziano Scarcelli on 08/02/23.
-//  Copyright © 2023 Rafael Damasceno. All rights reserved.
-//
-
 import Foundation
 import SwiftyChrono
 
-
-
-class NLPDateParser{
+class NLPDateParser {
     let parser: Chrono
     let userPreferences: UserPreferences
     var isLanguageSupported: Bool {
         Chrono.preferredLanguage != nil
     }
-    var isTimeDefined: Bool = false
-    var isDateDefined: Bool = false
+    var isTimeDefined = false
+    var isDateDefined = false
 
-    init(){
+    init() {
         self.parser = Chrono()
         self.userPreferences = UserPreferences.instance
-        //TODO: for now this is set only when the parser is initialized, is has to be changed in order to change every time the language is changes in the application by the user
+        // TODO: for now this is set only when the parser is initialized, is has to be changed in order to
+        // change every time the language is changes in the application by the user
         Chrono.preferredLanguage = self.getPreferredLanguage(from: rmbCurrentLocale().languageCode)
     }
     
     private func getPreferredLanguage(from languageCode: String?) -> SwiftyChrono.Language? {
-        // SwiftyChrono in date 11 Febraury 2023 only supports the listed languages, in case the language is not supported from SwiftyChrono, then the NLP Date parser won't be available
-        guard let languageCode = languageCode else {return nil}
-        switch languageCode{
-        case "en": return .english
-        case "fr": return .french
-        case "ja": return .japanese
-        case "es": return .spanish
-        case "zh": return .chinese
-        default: return nil
+        // SwiftyChrono in date 11 Febraury 2023 only supports the listed languages, in case the language is
+        // not supported from SwiftyChrono, then the NLP Date parser won't be available
+        guard let languageCode else { return nil }
+        switch languageCode {
+        case "en":
+            return .english
+        case "fr":
+            return .french
+        case "ja":
+            return .japanese
+        case "es":
+            return .spanish
+        case "zh":
+            return .chinese
+        default:
+            return nil
         }
     }
     
-    func buildDate(from string: String) -> (Date, String)?{
-        let parsedResults = parser.parse(text: string, refDate: Date(), opt:[.forwardDate: 1])
-        if parsedResults.count == 0 {return nil}
+    func buildDate(from string: String) -> (Date, String)? {
+        let parsedResults = parser.parse(text: string, refDate: Date(), opt: [.forwardDate: 1])
+        if parsedResults.isEmpty {return nil}
         let startDateInfo: [ComponentUnit: Int] = parsedResults[0].start.knownValues
         let dateRelatedText: String = parsedResults[0].text
         let todayComponents = Calendar.current.dateComponents([.year, .month, .day], from: Date())
@@ -54,37 +52,26 @@ class NLPDateParser{
         startDateComponents.minute = startDateInfo[SwiftyChrono.ComponentUnit.minute]
         self.isTimeDefined = checkTimeDefined(from: startDateComponents)
         self.isDateDefined = checkDateDefined(from: startDateComponents)
-        let userCalendar = Calendar(identifier: .gregorian) //TODO: idk if this has to be changed with user's calendar
+        let userCalendar = Calendar(identifier: .gregorian) // TODO: idk if this has to be changed with user's calendar
         let finalDateTime = userCalendar.date(from: startDateComponents)
-        guard let finalDateTime = finalDateTime else {return nil}
+        guard let finalDateTime else { return nil }
         if finalDateTime < Date() {
             // If the date is in the past, and the time it's not defined, then it's not valid
-            if !isTimeDefined {
-                return nil
-            }
+            if !isTimeDefined { return nil }
             // Otherwise, we assume that the user is referring to the time for the next day
-            startDateComponents.day! += 1
+            startDateComponents.day? += 1
             let finalDateTime = userCalendar.date(from: startDateComponents)
-            guard let finalDateTime = finalDateTime else {return nil}
+            guard let finalDateTime else { return nil }
             return (finalDateTime, dateRelatedText)
         }
         return (finalDateTime, dateRelatedText)
     }
     
-    func checkTimeDefined(from date: DateComponents) -> Bool{
-        guard let _ = date.minute, let _ = date.hour else {
-            return false
-        }
-        return true
+    func checkTimeDefined(from date: DateComponents) -> Bool {
+        return date.minute != nil && date.hour != nil
     }
     
-    func checkDateDefined(from date: DateComponents) -> Bool{
-        guard let _ = date.year, let _ = date.month, let _ = date.day else {
-            return false
-        }
-        return true
+    func checkDateDefined(from date: DateComponents) -> Bool {
+        return date.year != nil && date.month != nil
     }
-    
 }
-
-
