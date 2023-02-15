47dfbb456bfffb7e727185c720c0783303b027da
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-02-15 07:57:35 +0100

    Removed useless files

--- MyPlayground.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-import Cocoa
-import SwiftyChrono
-
-let parser = Chrono()
-Chrono.preferredLanguage = .english
-let result = parser.parse(text: "Task 12 aug", refDate: Date())
-print(result[0].text)
