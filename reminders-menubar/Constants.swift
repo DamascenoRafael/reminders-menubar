9c1851cdba44de66e154a583af7ec43529860f85
Rafael Damasceno <damscn@gmail.com>
2023-04-07 00:57:13 -0300

    Updating appName constant

--- reminders-menubar/Constants.swift
+++ reminders-menubar/Constants.swift
@@ -9,7 +9,7 @@ struct AppConstants {
         return "v\(bundleVersion)"
     }()
     
-    static let appName = "Reminders Menu Bar"
+    static let appName = "Reminders MenuBar"
     static let mainBundleId = "br.com.damascenorafael.reminders-menubar"
     static let launcherBundleId = "br.com.damascenorafael.RemindersLauncher"
 }
