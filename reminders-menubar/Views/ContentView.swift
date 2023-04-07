cea4a4ffb484d958342b149b3abd1b52a8f24d9a
Rafael Damasceno <damscn@gmail.com>
2023-04-07 15:56:58 -0300

    Adding ContentView preview again

--- reminders-menubar/Views/ContentView.swift
+++ reminders-menubar/Views/ContentView.swift
@@ -57,8 +57,8 @@ struct ContentView: View {
     }
 }
 
-// struct ContentView_Previews: PreviewProvider {
-//     static var previews: some View {
-//         ContentView().environmentObject(RemindersData())
-//     }
-// }
+ struct ContentView_Previews: PreviewProvider {
+     static var previews: some View {
+         ContentView().environmentObject(RemindersData())
+     }
+ }
