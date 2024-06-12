d240fc4db1c010ea9b19a6c4a62c7492f33c94b8
Mia yun Ruse <mia@yunru.se>
2024-06-12 01:23:13 +0100

    quick refactor: simplify menuBarCounterMenu

--- reminders-menubar/Models/RmbMenuBarCounterType.swift
+++ reminders-menubar/Models/RmbMenuBarCounterType.swift
@@ -1,6 +1,19 @@
-enum RmbMenuBarCounterType: String, Codable {
+enum RmbMenuBarCounterType: String, Codable, CaseIterable {
     case due
     case today
     case allReminders
     case disabled
+    
+    var title: String {
+        switch self {
+        case .due:
+            return rmbLocalized(.showMenuBarDueCountOptionButton)
+        case .today:
+            return rmbLocalized(.showMenuBarTodayCountOptionButton)
+        case .allReminders:
+            return rmbLocalized(.showMenuBarAllRemindersCountOptionButton)
+        case .disabled:
+            return rmbLocalized(.showMenuBarNoCountOptionButton)
+        }
+    }
 }
