fa817a46a37a98cc1cf1faab51055f64abdd12fd
Rafael Damasceno <damscn@gmail.com>
2024-06-17 01:49:02 -0300

    String catalog (xcstrings) related changes

--- reminders-menubar/AppCommands.swift
+++ reminders-menubar/AppCommands.swift
@@ -2,37 +2,49 @@ import SwiftUI
 
 struct AppCommands: Commands {
     @CommandsBuilder var body: some Commands {
-        CommandMenu("Edit") {
+        CommandMenu(Text(verbatim: "Edit")) {
             // NOTE: macOS 13.0 already has the below shortcuts for TextField.
             // Shortcuts only need to be registered for versions earlier than macOS 13.0.
             if #unavailable(macOS 13.0) {
-                Button("Select All") {
+                Button {
                     NSApp.sendAction(#selector(NSText.selectAll(_:)), to: nil, from: nil)
+                } label: {
+                    Text(verbatim: "Select All")
                 }
                 .keyboardShortcut(KeyEquivalent("a"), modifiers: .command)
                 
-                Button("Cut") {
+                Button {
                     NSApp.sendAction(#selector(NSText.cut(_:)), to: nil, from: nil)
+                } label: {
+                    Text(verbatim: "Cut")
                 }
                 .keyboardShortcut(KeyEquivalent("x"), modifiers: .command)
                 
-                Button("Copy") {
+                Button {
                     NSApp.sendAction(#selector(NSText.copy(_:)), to: nil, from: nil)
+                } label: {
+                    Text(verbatim: "Copy")
                 }
                 .keyboardShortcut(KeyEquivalent("c"), modifiers: .command)
                 
-                Button("Paste") {
+                Button {
                     NSApp.sendAction(#selector(NSText.paste(_:)), to: nil, from: nil)
+                } label: {
+                    Text(verbatim: "Paste")
                 }
                 .keyboardShortcut(KeyEquivalent("v"), modifiers: .command)
                 
-                Button("Undo") {
+                Button {
                     NSApp.sendAction(Selector(("undo:")), to: nil, from: nil)
+                } label: {
+                    Text(verbatim: "Undo")
                 }
                 .keyboardShortcut(KeyEquivalent("z"), modifiers: .command)
                 
-                Button("Redo") {
+                Button {
                     NSApp.sendAction(Selector(("redo:")), to: nil, from: nil)
+                } label: {
+                    Text(verbatim: "Redo")
                 }
                 .keyboardShortcut(KeyEquivalent("z"), modifiers: [.command, .shift])
             }
