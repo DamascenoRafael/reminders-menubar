b6a280ccc1945ab754f94c6aa04fd187eb058ca3
Rafael Damasceno <damscn@gmail.com>
2026-03-02 23:05:56 -0300

    Update popover defaultSize

--- reminders-menubar/MainPopoverSizing.swift
+++ reminders-menubar/MainPopoverSizing.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum MainPopoverSizing {
-    static let defaultSize = NSSize(width: 340, height: 460)
+    static let defaultSize = NSSize(width: 300, height: 540)
     static let minSize = NSSize(width: 300, height: 320)
     static let maxSize = NSSize(width: 600, height: 1_000)
     static let minWidthPadding: CGFloat = 80
