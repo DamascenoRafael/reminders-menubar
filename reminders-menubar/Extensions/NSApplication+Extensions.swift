0aff664825e76a1e1aff9294fc72b2420a737821
Rafael Damasceno <damscn@gmail.com>
2026-03-18 00:13:35 -0300

    Make sure to close popover when opening app settings

--- reminders-menubar/Extensions/NSApplication+Extensions.swift
+++ reminders-menubar/Extensions/NSApplication+Extensions.swift
@@ -8,6 +8,7 @@ extension NSApplication {
     @MainActor
     func openAppSettings(tab: SettingsTab = .general) {
         SettingsView.initialTab = tab
+        AppDelegate.shared.popover.performClose(nil)
 
         if #available(macOS 14.0, *) {
             // Note: Post a notification that the hidden helper view (SettingsOpenerView) will pick up.
