1d03c140d356d97251b17d336c75c324fd840dd0
Rafael Damasceno <damscn@gmail.com>
2026-03-20 00:58:40 -0300

    Improve RmbIcon image logic and name representation

--- reminders-menubar/Models/RmbIcon.swift
+++ reminders-menubar/Models/RmbIcon.swift
@@ -20,10 +20,43 @@ enum RmbIcon: String, CaseIterable {
     }
     
     var image: NSImage {
-        return NSImage(named: self.rawValue) ?? NSImage(systemSymbolName: self.rawValue, accessibilityDescription: nil)!
+        if let assetImage = NSImage(named: self.rawValue) {
+            return assetImage
+        }
+        if let sfSymbol = NSImage(systemSymbolName: self.rawValue, accessibilityDescription: name) {
+            return sfSymbol
+        }
+        return Self.defaultIcon.image
     }
 
     var name: String {
-        return self.rawValue
+        switch self {
+        case .note1:
+            return "Note 1"
+        case .note2:
+            return "Note 2"
+        case .note3:
+            return "Note 3"
+        case .note4:
+            return "Note 4"
+        case .note5:
+            return "Note 5"
+        case .bell1:
+            return "Bell 1"
+        case .bell2:
+            return "Bell 2"
+        case .reminder1:
+            return "Reminder 1"
+        case .reminder2:
+            return "Reminder 2"
+        case .reminder3:
+            return "Reminder 3"
+        case .sfsymbols1:
+            return "Checklist"
+        case .sfsymbols2:
+            return "Circle"
+        case .smalldot:
+            return "Small Dot"
+        }
     }
 }
